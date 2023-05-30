54cd6638beab304a1f752dc04bbbfe0a5d4e5039
Alberto-Ibarra <alberto.e.ibarra@gmail.com>
2023-05-30 12:15:10 -0700

    learned ngFor & ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  isVisible: boolean = true
+
+  hideList(){
+    this.isVisible = !this.isVisible
+  }
+
   title = 'angular-my-turn';
+  friendslist = [
+    {
+      name: 'Albert',
+      age: 31
+    },
+    {
+      name: 'Dylan',
+      age: 8
+    },
+    {
+      name: 'Daniel',
+      age: 29
+    }
+  ]
 }
